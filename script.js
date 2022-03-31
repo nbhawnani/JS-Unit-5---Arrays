aa64ce80e0a7d1700a8c6bf309430e3eda268e11
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-03-31 13:49:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let myArray = ["bread", "dish soap", "detergent",12,34,true, false];
+
+let colors = ["red", "blue", "green"];
+
+const numbers=[1,2,3,4];
+
+console.log(myArray,colors);
+document.getElementById("demo1").innerHTML=myArray+"<br><br>"+colors+"<br>" ;
+
+document.getElementById("demo2").innerHTML="<b>"+numbers+"</b>";
+
+document.getElementById("demo3").innerHTML=colors[2];
